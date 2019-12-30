5863f373fbf6320d2ee218fc51da7a17d8cbded3
pran-p <hsr.pranav@gmail.com>
2019-12-30 16:56:58 +0530

    Created a simple port scanner

--- script.py
+++ script.py
@@ -1,9 +1,37 @@
-# This is a basic skeleton script
-import <module1>, <module2>
-def myFunction():
-    pass
+"""This is a simple version of a port scanner built using the concept of sockets in python"""
+import socket
+import sys
+def portScanner(lowerRange, higherRange,ip):
+    a=[]
+    for i in range(lowerRange,higherRange+1):
+        try:
+            # pass
+            print ("[+] 4tt3mpting t0 c0nn3ct t0 "+ str(ip)+":"+str(i))
+            s=socket.socket()
+            # print(ip,i)
+            s.connect((ip,i))
+            banner=s.recv(1024)
+            if banner:
+                print ("    [-] Port"+str(i)+" open:"+str(banner.decode()))
+                a.append((banner,i))
+            s.close()
+
+        except :
+            pass
+    return a
 def main():
-    myFunction()
+    scriptName=sys.argv[0]
+    lowerRange=int(sys.argv[1])
+    higherRange=int(sys.argv[2])
+    ip=sys.argv[3]
+    print ("[+] Script name: "+str(scriptName))
+    print("[+] Lower range is:"+str(lowerRange))
+    print("[+] Higher Range is:"+str(higherRange))
+    openPort=portScanner(lowerRange, higherRange, ip)
+    des="+"*100
+    print(des)
+    print("Summary")
+    print("Open ports are:", openPort)
+    print(des)
 if __name__ == '__main__':
     main()
-    
