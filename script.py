3830cbaf91fe2fc64b82884fc7d7734c576089c5
pran-p <hsr.pranav@gmail.com>
2019-12-30 20:04:51 +0530

    Completely working port scanner with three functionalities

--- script.py
+++ script.py
@@ -6,12 +6,10 @@ import argparse
 """This function is used to scan a given ip for a given open port"""
 def portScanSingle(port,ip):
     s=socket.socket()
-    a=[]
     try:
         print ("[+] 4tt3mpting t0 c0nn3ct t0 "+ str(ip)+":"+str(port))
-        s.connect((ip,port))
-        print ("    [-] Port"+str(i)+" open:")
-        a.append((i,ip))
+        s.connect((ip,int(port)))
+        print ("    [.] Port open: "+str(port))
         s.close()
         return 1
     except:
@@ -26,7 +24,7 @@ def portScanSingleIp(port,ip):
         try:
             test=portScanSingle(i,ip)
             if test==1:
-                a.append(i,ip)
+                a.append((i,ip))
         except:
             pass
     return a
@@ -58,18 +56,53 @@ def portScanPortIp(port,ip):
     return b
 
 def main():
-    scriptName=sys.argv[0]
-    lowerRange=int(sys.argv[1])
-    higherRange=int(sys.argv[2])
-    ip=sys.argv[3].split(",")
-    print ("[+] Script name: "+str(scriptName))
-    print("[+] Lower range is:"+str(lowerRange))
-    print("[+] Higher Range is:"+str(higherRange))
-    openPort=portScanner(lowerRange, higherRange, ip)
-    des="+"*100
-    print(des)
-    print("Summary")
-    print("Open ports are:", openPort)
-    print(des)
+    parser=argparse.ArgumentParser(description="P0rt M4pp3r: A simple port scanner tool...")
+    parser.add_argument('--options',dest='options',help='1-single port ip, 2-multiple port on single ip, 3-range of ports on multiple ip ',type=int, metavar='opt', required=True )
+    parser.add_argument('-p','--port',dest='port',help='port number(comma separated in case of multiple). use in case of option 1,2', metavar='port')
+    parser.add_argument('-ip','--ip-address',dest='ip',help='ip address(comma separeted in case of multiple). use in case of option 1,2,3', metavar='ip')
+    parser.add_argument('-lp','--lower-range',dest='ll',help='lower port limit. use in case of option 3', metavar='port', type=int)
+    parser.add_argument('-hp','--higher-range',dest='hl',help='higher port limit. use in case of option 3', metavar='port', type=int)
+    args=parser.parse_args()
+    if args.options==1:
+        ans=portScanSingle(args.port,args.ip)
+        if ans==0:
+            print("Th3 p0rt is cl0s3d...")
+    elif args.options==2:
+        p=args.port.split(',')
+        ans=portScanSingleIp(p,args.ip)
+        print("The open ports are:",ans)
+    elif args.options==3:
+        address=args.ip.split(',')
+        if args.hl>65535:
+            print("Out of limit")
+        else:
+            ans=portScanner(args.ll,args.hl, address)
+        print("The open ports are:",ans)
+    else:
+        print("Invalid option")
+
+
+
+
+
+
+
+
+
+
+
+    # scriptName=sys.argv[0]
+    # lowerRange=int(sys.argv[1])
+    # higherRange=int(sys.argv[2])
+    # ip=sys.argv[3].split(",")
+    # print ("[+] Script name: "+str(scriptName))
+    # print("[+] Lower range is:"+str(lowerRange))
+    # print("[+] Higher Range is:"+str(higherRange))
+    # openPort=portScanner(lowerRange, higherRange, ip)
+    # des="+"*100
+    # print(des)
+    # print("Summary")
+    # print("Open ports are:", openPort)
+    # print(des)
 if __name__ == '__main__':
     main()
