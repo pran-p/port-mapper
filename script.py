30ac4d7966e8ecc878e1e6ce7190a4385a53e1a2
pran-p <hsr.pranav@gmail.com>
2019-12-30 17:19:24 +0530

    Extended portScanner for multiple IPs and made it more versatile

--- script.py
+++ script.py
@@ -1,29 +1,32 @@
 """This is a simple version of a port scanner built using the concept of sockets in python"""
 import socket
 import sys
-def portScanner(lowerRange, higherRange,ip):
+def portScanner(lowerRange, higherRange,hosts):
     a=[]
-    for i in range(lowerRange,higherRange+1):
-        try:
-            # pass
-            print ("[+] 4tt3mpting t0 c0nn3ct t0 "+ str(ip)+":"+str(i))
-            s=socket.socket()
-            # print(ip,i)
-            s.connect((ip,i))
-            banner=s.recv(1024)
-            if banner:
-                print ("    [-] Port"+str(i)+" open:"+str(banner.decode()))
-                a.append((banner,i))
-            s.close()
+    for ip in hosts:
+        for i in range(lowerRange,higherRange+1):
+            try:
+                # pass
+                print ("[+] 4tt3mpting t0 c0nn3ct t0 "+ str(ip)+":"+str(i))
+                s=socket.socket()
+                # print(ip,i)
+                s.connect((ip,i))
+                # banner=s.recv(1024)
+                # if banner:
+                #     print ("    [-] Port"+str(i)+" open:"+str(banner.decode()))
+                #     a.append((banner,i,ip))
+                print ("    [-] Port"+str(i)+" open:")
+                a.append((i,ip))
+                s.close()
 
-        except :
-            pass
+            except :
+                pass
     return a
 def main():
     scriptName=sys.argv[0]
     lowerRange=int(sys.argv[1])
     higherRange=int(sys.argv[2])
-    ip=sys.argv[3]
+    ip=sys.argv[3].split(",")
     print ("[+] Script name: "+str(scriptName))
     print("[+] Lower range is:"+str(lowerRange))
     print("[+] Higher Range is:"+str(higherRange))
